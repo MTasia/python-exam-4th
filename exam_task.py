74ece4c969efdba21410a1bd43013ccc8832a6cb
Taya Medvedeva <71186576+MTasia@users.noreply.github.com>
2021-12-20 11:24:24 +0300

    Comment print

--- exam_task.py
+++ exam_task.py
@@ -29,17 +29,17 @@ def count_steps(maze, start, end, n):
     while queue:
         curr = queue.popleft()
         mass1.append(curr)
-        print("curr", curr)
+        # print("curr", curr)
         curr_x, curr_y = curr[0], curr[1]
         visited[curr_x][curr_y] = True
         neighbours = possible_neighbours(curr_x, curr_y, maze, visited)
-        print(neighbours)
-        print()
+        # print(neighbours)
+        # print()
         for nb in neighbours:
             queue.append(nb)
             if nb == end:
                 return 0
-    print("for end")
+    # print("for end")
     j = end[0]
     i = end[1]
     # visited = [[False for j in range(n)] for i in range(n)]
@@ -50,21 +50,21 @@ def count_steps(maze, start, end, n):
     while queue:
         curr = queue.popleft()
         mass2.append(curr)
-        print("curr", curr)
+        # print("curr", curr)
         curr_x, curr_y = curr[0], curr[1]
         visited[curr_x][curr_y] = True
         neighbours = possible_neighbours(curr_x, curr_y, maze, visited)
-        print(neighbours)
-        print()
+        # print(neighbours)
+        # print()
         for nb in neighbours:
             queue.append(nb)
             if nb == end:
                 return 0
 
-    for i in visited:
-        print(*i)
-    print(mass1)
-    print(mass2)
+    # for i in visited:
+    #     print(*i)
+    # print(mass1)
+    # print(mass2)
 
     # Search min path
     min_path = (mass1[0][0] - mass2[0][0]) ** 2 + (mass1[0][1] - mass2[0][1]) ** 2
@@ -77,10 +77,10 @@ def count_steps(maze, start, end, n):
 
 
 def main():
-    n = 5
-    x1, y1 = 1, 1
-    x2, y2 = 5, 5
-    mass = [[0, 0, 0, 0, 1], [1, 1, 1, 1, 1], [0, 0, 1, 1, 1], [0, 0, 1, 1, 0], [0, 0, 1, 1, 0]]
+    n = 3
+    x1, y1 = 1, 3
+    x2, y2 = 3, 1
+    mass = [[0, 1, 0], [1, 0, 1], [0, 1, 0]]
 
     start = (x1 - 1, y1 - 1)
     end = (x2 - 1, y2 - 1)
